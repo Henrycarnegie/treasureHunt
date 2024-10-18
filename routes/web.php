b51185eb163fa5906e6f3ad5b7b42f9bd7210dce
fadhilfiroos@gmail.com <fadhilfiroos@gmail.com>
2024-10-18 23:44:28 +0700

    BE-feat: finish setup TALL stack dan menyesuaikan FE ke konfigurasi TALL stack, dan finish BE for login

--- routes/web.php
+++ routes/web.php
@@ -1,13 +1,37 @@
 <?php
 
+use App\Http\Controllers\Auth\EmailVerificationController;
+use App\Http\Controllers\Auth\LogoutController;
+use App\Livewire\Auth\Login;
+use App\Livewire\Auth\Passwords\Confirm;
+use App\Livewire\Auth\Passwords\Email;
+use App\Livewire\Auth\Passwords\Reset;
+use App\Livewire\Auth\Register;
+use App\Livewire\Auth\Verify;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/', function () {
-    return view('auth.login');
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| contains the "web" middleware group. Now create something great!
+|
+*/
+
+Route::view('/', 'homepage')->name('home');
+
+Route::middleware('guest')->group(function () {
+    Route::get('login', Login::class)
+        ->name('login');
 });
 
-Route::get('/homepage', function () {
-    return view('homepage');
+
+Route::middleware('auth')->group(function () {
+        Route::post('logout', LogoutController::class)
+        ->name('logout');
 });
 
 Route::get('/guru', function () {
--- tailwind.config.js
+++ tailwind.config.js
@@ -27,6 +27,24 @@ export default {
         18 : '18%',
       },
     },
-  },
-  plugins: [],
+    variants: {
+        extend: {
+            backgroundColor: ['active'],
+        }
+    },
+    content: [
+        './app/**/*.php',
+        './resources/**/*.html',
+        './resources/**/*.js',
+        './resources/**/*.jsx',
+        './resources/**/*.ts',
+        './resources/**/*.tsx',
+        './resources/**/*.php',
+        './resources/**/*.vue',
+        './resources/**/*.twig',
+    ],
+    plugins: [
+        require('@tailwindcss/forms'),
+        require('@tailwindcss/typography'),
+    ],
 }
