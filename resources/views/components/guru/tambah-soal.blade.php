4aea1a84b65189b19f94ac92426b5875ec0d8ca4
Henry Carnegie <henrycarnegie23@gmail.com>
2024-11-04 15:49:09 +0700

    FE-feat: Fixing logic tambah soal

--- resources/views/components/guru/tambah-soal.blade.php
+++ resources/views/components/guru/tambah-soal.blade.php
@@ -1,10 +1,7 @@
-@props(['responseType' => '', 'pekerjaan', 'type_question'=>''])
-<div
-    x-show="tambahSoalOpen"
-    x-transition.opacity x-cloak
-    @click="tambahSoalOpen = false"
-    class="fixed inset-0 z-[999] grid h-screen w-screen place-items-center bg-black bg-opacity-60 backdrop-blur-sm transition-opacity duration-300"
-    >
+@props(['responseType' => '', 'pekerjaan', 'type_question' => '', 'fullOption'=>''])
+
+<div x-show="tambahSoalOpen" x-transition.opacity x-cloak @click="tambahSoalOpen = false"
+    class="fixed inset-0 z-[999] grid h-screen w-screen place-items-center bg-black bg-opacity-60 backdrop-blur-sm transition-opacity duration-300">
     <div class="relative mx-auto w-full max-w-[24rem] rounded-lg overflow-hidden shadow-sm bg-white" @click.stop>
         <form wire:submit.prevent="simpanSoal">
             {{-- Modal Input --}}
@@ -14,18 +11,17 @@
                 <div class="w-full flex justify-end">
                     <x-icon icon="iconClose" @click="tambahSoalOpen = false"></x-icon>
                 </div>
-                <x-guru.field-tambah-soal responseType="{{ $responseType }}"></x-guru.field-tambah-soal>
+                <x-guru.field-tambah-soal :responseType="$responseType" :fullOption="$fullOption"></x-guru.field-tambah-soal>
             </div>
 
             <!-- Simpan Soal Button -->
             <div class="p-6 pt-0">
-                <button
-                    wire:click="setValue('{{ $pekerjaan }}', '{{ $type_question }}')" class="w-full rounded-md bg-indigo-600 py-2 px-4 border border-transparent text-center text-sm text-white transition-all shadow-md hover:shadow-lg focus:bg-indigo-700 focus:shadow-none active:bg-indigo-700 hover:bg-indigo-700 active:shadow-none disabled:pointer-events-none disabled:opacity-50 disabled:shadow-none"
+                <button wire:click="setValue('{{ $pekerjaan }}', '{{ $type_question }}')"
+                    class="w-full rounded-md bg-indigo-600 py-2 px-4 border border-transparent text-center text-sm text-white transition-all shadow-md hover:shadow-lg focus:bg-indigo-700 focus:shadow-none active:bg-indigo-700 hover:bg-indigo-700 active:shadow-none disabled:pointer-events-none disabled:opacity-50 disabled:shadow-none"
                     type="submit">
                     Simpan Soal
                 </button>
             </div>
         </form>
     </div>
-
 </div>
