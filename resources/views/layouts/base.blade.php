fd29c055d46497936da3dca00e0781479c24f09f
Henry Carnegie <henrycarnegie23@gmail.com>
2024-11-12 15:56:00 +0700

    FE-feat: Error on leaderboard view

--- resources/views/layouts/base.blade.php
+++ resources/views/layouts/base.blade.php
@@ -1,38 +1,40 @@
 <!DOCTYPE html>
 <html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
-    <head>
-        <meta charset="utf-8">
-        <meta name="viewport" content="width=device-width, initial-scale=1">
-        @hasSection('title')
 
-            <title>@yield('title') - {{ config('app.name') }}</title>
-        @else
-            <title>{{ config('app.name') }}</title>
-        @endif
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    @hasSection('title')
+        <title>@yield('title') - {{ config('app.name') }}</title>
+    @else
+        <title>{{ config('app.name') }}</title>
+    @endif
 
-        <!-- Favicon -->
-        <link rel="icon" href="{{ asset('img/logo.svg') }}">
+    <!-- Favicon -->
+    <link rel="icon" href="{{ asset('img/logo.svg') }}">
 
-        {{-- Font Inter --}}
-        <link rel="stylesheet" href="https://rsms.me/inter/inter.css">
+    {{-- Font Inter --}}
+    <link rel="stylesheet" href="https://rsms.me/inter/inter.css">
 
-        {{-- Font Bangers --}}
-        <link rel="preconnect" href="https://fonts.googleapis.com">
-        <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
-        <link href="https://fonts.googleapis.com/css2?family=Bangers&display=swap" rel="stylesheet">
+    {{-- Font Bangers --}}
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Bangers&display=swap" rel="stylesheet">
 
-        @vite(['resources/sass/app.scss', 'resources/js/app.js'])
-        @livewireStyles
-        @livewireScripts
+    @vite(['resources/sass/app.scss', 'resources/js/app.js'])
+    @livewireStyles
+    @livewireScripts
 
-        <!-- CSRF Token -->
-        <meta name="csrf-token" content="{{ csrf_token() }}">
-    </head>
+    <!-- CSRF Token -->
+    <meta name="csrf-token" content="{{ csrf_token() }}">
+</head>
 
-    <body class="font-sans antialiase">
-        @yield('body')
+<body class="font-sans antialiase">
+    @yield('body')
+    
+    <script src="https://cdn.jsdelivr.net/npm/echarts/dist/echarts.min.js"></script>
+    <script src="//cdn.jsdelivr.net/npm/sweetalert2@11"></script>
+    <x-livewire-alert::scripts />
+</body>
 
-        <script src="//cdn.jsdelivr.net/npm/sweetalert2@11"></script>
-        <x-livewire-alert::scripts />
-    </body>
 </html>
--- resources/views/livewire/murid/leaderboard.blade.php
+++ resources/views/livewire/murid/leaderboard.blade.php
@@ -1,3 +1,30 @@
-<div>
-    {{-- The whole world belongs to you. --}}
-</div>
+<div id="leaderboard-chart" style="width: 600px; height: 400px;"></div>
+<script>
+    document.addEventListener("livewire:load", function () {
+        var chartDom = document.getElementById('leaderboard-chart');
+        if (chartDom) {
+            var myChart = echarts.init(chartDom);
+
+            var option = {
+                title: {
+                    text: 'Leaderboard Chart'
+                },
+                tooltip: {},
+                legend: {
+                    data: ['Scores']
+                },
+                xAxis: {
+                    data: ['Player1', 'Player2', 'Player3', 'Player4', 'Player5']
+                },
+                yAxis: {},
+                series: [{
+                    name: 'Scores',
+                    type: 'bar',
+                    data: [120, 200, 150, 80, 70]
+                }]
+            };
+
+            myChart.setOption(option);
+        }
+    });
+</script>
