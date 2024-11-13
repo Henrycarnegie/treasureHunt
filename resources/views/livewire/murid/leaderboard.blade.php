5dcf2852c6154c3fd2d00865c1f8daaee1a6f0bd
Henry Carnegie <henrycarnegie23@gmail.com>
2024-11-13 12:44:22 +0700

    FE-feat: Create Leaderboard Page

--- resources/views/livewire/murid/leaderboard.blade.php
+++ resources/views/livewire/murid/leaderboard.blade.php
@@ -1,13 +1,37 @@
-<div id="main" style="width: 1200px; height: 800px;"></div>
+<div class="min-w-fit">
+    <div class="inline-flex w-full items-center justify-between px-4">
+        <a href="{{ route('murid.home') }}" class="text-slate-700 font-semibold font-base">Back</a>
+        <h1 class="text-2xl font-bold text-center py-8">Leaderboard</h1>
+        <a href="{{ route('logout') }}" class="text-red-500 font-semibold font-base">Logout</a>
+    </div>
+    <div id="main"
+        style=" margin-left: 10px;
+                width: 1200px;
+                height: 800px;
+                max-width: 100%;
+                @media (min-width: 640px) {
+                    max-width: 150%;
+                }
+                @media (min-width: 768px) {
+                    max-width: 180%;
+                }
+                @media (min-width: 1024px) {
+                    max-width: 250%
+                }
+                @media (min-width: 1280px) {
+                    max-width: 300%
+                }"
+    ></div>
+</div>
 <script type="text/javascript">
     var myChart = echarts.init(document.getElementById('main'));
 
-    var data = [120, 200, 150, 80, 70]; // Sample data
-    var categories = ['A', 'B', 'C', 'D', 'E']; // Categories
+    var data = [0, 0, 0, 0]; // Sample data
+    var categories = ['Polisi', 'Detektif', 'Petani', 'Nelayan']; // Categories
 
     var option = {
         xAxis: {
-            max: 'dataMax'
+            max: 'dataMax',
         },
         yAxis: {
             type: 'category',
@@ -15,20 +39,23 @@
             inverse: true,
             animationDuration: 300,
             animationDurationUpdate: 300,
-            max: 2 // Display only the largest 2 bars
+            max: 3
         },
         series: [{
             realtimeSort: true,
             name: 'Values',
             type: 'bar',
             data: data,
+            yAxis: {
+                padding: 100
+            },
             label: {
                 show: true,
                 position: 'right',
                 valueAnimation: true
             },
             itemStyle: {
-                color: 'green' // Set bar color
+                color: 'blue' // Set bar color
             }
         }],
         animationDuration: 0,
@@ -36,20 +63,20 @@
         animationEasing: 'linear',
         animationEasingUpdate: 'linear'
     };
-
     myChart.setOption(option);
 
     // Function to update data for racing effect
-    // function run() {
-    //     for (var i = 0; i < data.length; ++i) {
-    //         data[i] += Math.round(Math.random() * 200); // Randomly update data
-    //     }
-    //     myChart.setOption({
-    //         series: [{
-    //             data: data
-    //         }]
-    //     });
-    // }
+    function run() {
+        for (var i = 0; i < data.length; ++i) {
+            data[i] += Math.round(Math.random() * 200); // Randomly update data
+        }
+        myChart.setOption({
+            series: [{
+                data: data,
+            }]
+        });
+    }
 
-    // setInterval(run, 3000); // Update every 3 seconds
+    setInterval(run, 3000); // Update every 3 seconds
+
 </script>
