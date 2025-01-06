b6edae8c4d78b2ddf2fb8e06f1499cbe543a563e
Fadhil-Firoos <fadhilfiroos@gmail.com>
2025-01-06 22:33:03 +0700

    update cek info php

--- routes/web.php
+++ routes/web.php
@@ -40,6 +40,11 @@ use Illuminate\Support\Facades\Route;
 Route::middleware('guest')->group(function () {
     Route::get('/', Login::class)
     ->name('login');
+
+    Route::get('/info', function () {
+        return phpinfo();
+    });
+
 });
 
 Route::middleware('auth')->group(function () {
