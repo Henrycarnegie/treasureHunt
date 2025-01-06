617a114ae5d38c8526f6058258e89c3b04a0448c
Fadhil-Firoos <fadhilfiroos@gmail.com>
2025-01-06 13:23:15 +0700

    update validasi view soal image

--- resources/views/components/murid/layout-soal.blade.php
+++ resources/views/components/murid/layout-soal.blade.php
@@ -3,9 +3,11 @@
 <div class="bg-white rounded-md bg-clip-padding backdrop-filter backdrop-blur-md border-gray-200 shadow-lg px-8 py-6 min-w-full min-h-80">
     <h1 class="text-3xl font-bold  mb-4 text-gray-800">Soal no {{ $infoSoal }}</h1>
     <h1 class="text-lg font-medium  text-slate-900">{{ $soal }}</h1>
-    <div class="flex w-full justify-center items-center">
-        <img src="{{ asset('storage/soal_level'.$infoLevel.'/'.$image) }}" alt="Gambar Soal" class="w-full max-w-max h-auto max-h-96">
-    </div>
+    @if ($image)
+        <div class="flex w-full justify-center items-center">
+            <img src="{{ asset('storage/soal_level'.$infoLevel.'/'.$image) }}" alt="Gambar Soal" class="w-full max-w-max h-auto max-h-96">
+        </div>
+    @endif
 </div>
 
 
